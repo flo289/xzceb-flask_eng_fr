857174f79b7b21dba74e81aef861133514a9cfde
flo289 <125163793+flo289@users.noreply.github.com>
2023-02-14 15:10:50 -0500

    Update tests.py

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,19 +1,17 @@
+from translator import french_to_english
+from translator import english_to_french
 import unittest
 
-from translator import french_to_english,english_to_french
+class TestenglishToFrench(unittest.TestCase): 
+    def test1(self): 
+        self.assertEqual(english_to_french(''), '')
+        self.assertEqual(english_to_french('Hello'), 'Bonjour')
+        self.assertNotEqual(english_to_french('Goodbye'), 'Bonjour')
+        
+class TestfrenchToEnglish(unittest.TestCase): 
+    def test2(self): 
+        self.assertEqual(french_to_english(''), '')
+        self.assertEqual(french_to_english('Bonjour'), 'Hello')
+        self.assertNotEqual(french_to_english('Au revoir'), 'Hello')
 
-class test_f2e(unittest.TestCase): 
-    def test1(self): 
-        self.assertNotEqual(french_to_english(0), 0) 
-    def test2(self):
-        self.assertEqual(french_to_english('Nous'), 'We') 
-    def test3(self):
-         self.assertEqual(french_to_english('Bonjour'), 'Hello') 
-class test_e2f(unittest.TestCase): 
-    def test1(self): 
-        self.assertNotEqual(english_to_french(0), 0) 
-    def test2(self):
-        self.assertEqual(english_to_french('We'), 'Nous') 
-    def test3(self):
-         self.assertEqual(english_to_french('Hello'), 'Bonjour') 
 unittest.main()
